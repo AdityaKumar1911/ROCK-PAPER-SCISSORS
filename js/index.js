fb36be84f198fd1be132da7e580b37008baf255b
Aditya Kumar <aditya.kumar2020@vitstudent.ac.in>
2023-09-20 22:32:35 +0530

    ROCK-PAPER-SCISSORS

--- js/index.js
+++ js/index.js
@@ -1,11 +1,14 @@
 const handOptions = {
-  "rock": "/assets/Rock.png",
-  "paper": "/assets/Paper.png",
-  "scissors": "/assets/Scissors.png"
+  "rock": "/assets/rockbg.png",
+  "paper": "/assets/paperbg.png",
+  "scissors": "/assets/ssbg.png"
 }
 
 let CSCORE = 0;
 let SCORE = 0;
+let allscore = localStorage.getItem("score") || [
+
+]
 // let COMSCORE = 0;
 
 const pickUserHand = (hand) => {
@@ -30,15 +33,20 @@ const pickComputerHand = (hand) => {
     
     referee(hand, cpHand);
 };
+localStorage.setItem('comscore', 'addd' )
 
 const referee = (userHand, cpHand) => {
   if (userHand == "paper" && cpHand == "scissors") {
     setDecision("YOU LOSE!");
     setScoree(CSCORE + 1);
+    localStorage.setItem('comscore',CSCORE + 1 )
+    alert("aditya");
   }
   if (userHand == "paper" && cpHand == "rock") {
     setDecision("YOU WIN!");
     setScore(SCORE + 1);
+    localStorage.setItem("yourscore",SCORE + 1 )
+   
   }
   if (userHand == "paper" && cpHand == "paper") {
     setDecision("It's a tie!");
@@ -46,10 +54,12 @@ const referee = (userHand, cpHand) => {
   if (userHand == "rock" && cpHand == "scissors") {
     setDecision("YOU WIN!");
     setScore(SCORE + 1);
+    localStorage.setItem("yourscore",SCORE + 1 )
   }
   if (userHand == "rock" && cpHand == "paper") {
     setDecision("YOU LOSE!");
     setScoree(CSCORE + 1);
+    localStorage.setItem("comscore",CSCORE + 1 )
   }
   if (userHand == "rock" && cpHand == "rock") {
     setDecision("It's a tie!");
@@ -60,12 +70,16 @@ const referee = (userHand, cpHand) => {
   if (userHand == "scissors" && cpHand == "rock") {
     setDecision("YOU LOSE!");
     setScoree(CSCORE + 1);
+    localStorage.setItem("comscore",CSCORE + 1 )
   }
   if (userHand == "scissors" && cpHand == "paper") {
     setDecision("YOU WIN!");
     setScore(SCORE + 1);
+    localStorage.setItem("yourscore",SCORE + 1 )
+    
   }
 };
+
 
 const restartGame = () => {
   let contest = document.querySelector(".contest");
@@ -84,11 +98,22 @@ const setDecision = (decision) => {
 const setScore = (newScore) => {
   SCORE = newScore;
   document.querySelector(".score h1").innerText = newScore;
+
 }
 const setScoree = (newScoree) =>{
   CSCORE = newScoree;
   document.querySelector(".cscore h1").innerText = newScoree;
+  
 }
 
+
+function myFunction() {
+  var x = document.getElementById("notification");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
 
 
